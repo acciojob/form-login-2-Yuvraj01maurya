0cf532c5f5e4d37a520d90ccccc59f70bdb1e41f
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-11-14 20:17:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.getElementById("myForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  const phoneNumber = document.getElementById("phoneNumber").value;
+  const email = document.getElementById("email").value;
+
+  const message = `First Name: ${firstName}\nLast Name: ${lastName}\nPhone Number: ${phoneNumber}\nEmail ID: ${email}`;
+  
+  alert(message);
+});
